862a3fe710e18ac09c3e47fd7e7b081aa0e2f561
DOL-DOM <haesolj@naver.com>
2025-04-30 08:20:45 +0900

    Backup: local, merge

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ from PIL import Image
 import os, json
 import torchvision.transforms as T
 import torch
+import random
 
 class VizWizGroundingDataset(Dataset):
     def __init__(self, json_path, image_root, mask_root=None, image_size=(224, 224), is_test=False):
@@ -13,29 +14,55 @@ class VizWizGroundingDataset(Dataset):
         self.image_size = image_size
         self.entries = list(self.data.items())
 
-        self.image_tf = T.Compose([
-            T.Resize(image_size),
-            T.ToTensor()
-        ])
-        self.mask_tf = T.Compose([
-            T.Resize(image_size),
-            T.ToTensor()
-        ])
+        # train/val 구분해서 transform 설정
+        if not self.is_test:
+            self.image_tf = T.Compose([
+                T.Resize((256, 256)),
+                T.RandomCrop(image_size),
+                T.RandomHorizontalFlip(),
+                T.ToTensor()
+            ])
+            self.mask_tf = T.Compose([
+                T.Resize((256, 256)),
+                T.RandomCrop(image_size),
+                T.RandomHorizontalFlip(),
+                T.ToTensor()
+            ])
+        else:
+            self.image_tf = T.Compose([
+                T.Resize(image_size),
+                T.ToTensor()
+            ])
+            self.mask_tf = T.Compose([
+                T.Resize(image_size),
+                T.ToTensor()
+            ])
 
     def __len__(self):
         return len(self.entries)
 
     def __getitem__(self, idx):
         filename, meta = self.entries[idx]
-        image = self.image_tf(Image.open(os.path.join(self.image_root, filename)).convert("RGB"))
+        
+        image = Image.open(os.path.join(self.image_root, filename)).convert("RGB")
         question = meta["question"]
         answer = meta.get("most_common_answer", "")
         text = f"Q: {question} A: {answer}" if answer else f"Q: {question}"
 
-        if not self.is_test and self.mask_root:
+        if self.mask_root:
             mask_path = os.path.join(self.mask_root, filename.replace(".jpg", ".png"))
-            mask = self.mask_tf(Image.open(mask_path).convert("L"))
+            mask = Image.open(mask_path).convert("L")
         else:
-            mask = torch.zeros(1, *self.image_size)
+            mask = Image.new("L", self.image_size)
+
+        # training일 때만 rotation 적용
+        if not self.is_test:
+            angle = random.choice([0, 90, 180, 270])
+            if angle != 0:
+                image = image.rotate(angle)
+                mask = mask.rotate(angle)
+
+        image = self.image_tf(image)
+        mask = self.mask_tf(mask)
 
         return {"image": image, "text": text, "mask": mask, "filename": filename}
