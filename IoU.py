30d99013de226cc44f5caea679252103cb6e917b
yjh9929 <yjh9929@cau.ac.kr>
2025-05-01 13:30:55 +0000

    small update

--- IoU.py
+++ IoU.py
@@ -14,7 +14,7 @@ mask_dir = "data/vizwiz/binary_masks_png/val"
 
 # 1. 모델 로드
 model = GroundingModel()
-model.load_state_dict(torch.load("outputs/clip-vit-L-p14-336-wo-sigmoid_epoch.pt"))
+model.load_state_dict(torch.load("outputs/clip-vit-L-p14-336-wo-sigmoid_epoch3.pt"))
 model.eval().cuda()
 
 # 2. val json 불러오기
